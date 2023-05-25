f79a9d671056844d8cf31a0fc54e987d6e7c5168
Your Name <you@example.com>
2023-05-25 01:15:08 -0700

    add file_type to admin&serializers

--- products/serializers.py
+++ products/serializers.py
@@ -10,9 +10,12 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class FileSerializer(serializers.ModelSerializer):
+    file_type = serializers.SerializerMethodField()
     class Meta:
         model = File
-        fields = ('id', 'title', 'file')
+        fields = ('id', 'title', 'file', 'file_type')
+    def get_file_type(self, obj):
+        return obj.get_file_type_display()
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
